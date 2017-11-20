76ea5894420e765d7728642d7c08f650bdf62abd
manytostao <manytostao@gmail.com>
2017-11-19 21:29:31 -0500

    Create users module translation file

---
+++ resources/lang/es/users.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Osmany Torres Leyva
+ * Date: 18/11/2017
+ * Time: 16:23
+ */
+
+return [
+    'module' => 'Cuentas de usuario',
+    'object' => 'Usuario del sistema'
+];
